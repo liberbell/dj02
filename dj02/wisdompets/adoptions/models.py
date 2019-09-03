7c7492df67b87b7a28e20b853397eedaba2857fb
Liberbell <liberbell@gmail.com>
2019-09-04 07:48:22 +0900

    models2

--- dj02/wisdompets/adoptions/models.py
+++ dj02/wisdompets/adoptions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    speices = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
