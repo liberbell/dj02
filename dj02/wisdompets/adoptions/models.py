47badbdbc4f50357483a81d8496c2cfe55022199
Liberbell <liberbell@gmail.com>
2019-09-05 07:53:41 +0900

    models1

--- dj02/wisdompets/adoptions/models.py
+++ dj02/wisdompets/adoptions/models.py
@@ -5,7 +5,7 @@ class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
-    speices = models.CharField(max_length=30)
+    species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
