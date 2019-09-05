ebfed5b44828d00ce823da10e3526cb440d801ce
Liberbell <liberbell@gmail.com>
2019-09-06 07:51:59 +0900

    urls3

--- dj02/wisdompets/wisdompets/urls.py
+++ dj02/wisdompets/wisdompets/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from adoptions import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
