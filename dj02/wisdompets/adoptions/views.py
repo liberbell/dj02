832665427de542a91619d88b0346f2eedf189e67
Liberbell <liberbell@gmail.com>
2019-09-06 07:48:55 +0900

    views3

--- dj02/wisdompets/adoptions/views.py
+++ dj02/wisdompets/adoptions/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<p>home view</p>')
+
+def pet_detail(request):
+    return HttpResponse('<p>detail view</p>')
