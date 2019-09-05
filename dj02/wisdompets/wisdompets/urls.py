6b41a17cde2e9a64bf902b0329ac6d21d824758e
Liberbell <liberbell@gmail.com>
2019-09-06 07:45:40 +0900

    urls2

--- dj02/wisdompets/wisdompets/urls.py
+++ dj02/wisdompets/wisdompets/urls.py
@@ -19,4 +19,6 @@ from adoptions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
