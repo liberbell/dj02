9c3518cb5632dfd0be0f63d937067d5ead1a666b
Liberbell <liberbell@gmail.com>
2019-09-07 11:12:14 +0900

    settings1

--- dj02/wisdompets/wisdompets/settings.py
+++ dj02/wisdompets/wisdompets/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bkavip4643rges03$4au$i756urslh+p+ox9wh&gp7ad&nl^ze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
