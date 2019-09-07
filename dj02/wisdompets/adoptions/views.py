20a1c0b6da6bed440f32feccef6d71280589d46f
Liberbell <liberbell@gmail.com>
2019-09-07 11:04:13 +0900

    views1

--- dj02/wisdompets/adoptions/views.py
+++ dj02/wisdompets/adoptions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pet
 
 # Create your views here.
 def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
     return HttpResponse('<p>home view</p>')
 
 def pet_detail(request, id):
-    return HttpResponse('<p>detail view with the id {}</p>'.format(id))
+    try:
+        pet = pet.objects.get(id=id)
+    except PetDoesNotExists:
+        raise Http404('Pet not found')
+    # return HttpResponse('<p>detail view with the id {}</p>'.format(id))
+
+    return render(request, 'pet_detail.html', {'pet': pet})
