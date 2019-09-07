9f84a8b5ecdb589292cfe3dada29585dfaefa52d
Liberbell <liberbell@gmail.com>
2019-09-07 11:08:26 +0900

    views3

--- dj02/wisdompets/adoptions/views.py
+++ dj02/wisdompets/adoptions/views.py
@@ -10,7 +10,7 @@ def home(request):
 
 def pet_detail(request, id):
     try:
-        pet = pet.objects.get(id=id)
+        pet = Pet.objects.get(id=id)
     except PetDoesNotExists:
         raise Http404('Pet not found')
     # return HttpResponse('<p>detail view with the id {}</p>'.format(id))
